7b6f20eaf91eed2d0b9343d4785022906e3216fd
jeyakoroll <jeyakoroll@gmail.com>
2018-07-27 08:24:20 +0300

    header static done

---
+++ source/js/main.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+  // $(window).on("scroll", function() {
+  //   var menu = $(".menu");
+  //   if ($(this).scrollTop() <= 75 && menu.hasClass("move")) {
+  //     menu.removeClass("move").addClass("top");
+  //   } else if ($(this).scrollTop() > 75 && menu.hasClass("top")) {
+  //     menu.removeClass("top").addClass("move");
+  //   }
+  // });
+  // //open popup
+  // $(".cd-popup-trigger").on("click", function(event) {
+  //   event.preventDefault();
+  //   $(".cd-popup").addClass("is-visible");
+  // });
+  // //close popup
+  // $(".cd-popup").on("click", function(event) {
+  //   if (
+  //     $(event.target).is(".cd-popup-close") ||
+  //     $(event.target).is(".cd-popup")
+  //   ) {
+  //     event.preventDefault();
+  //     $(this).removeClass("is-visible");
+  //   }
+  // });
+  // //close popup when clicking the esc keyboard button
+  // $(document).keyup(function(event) {
+  //   if (event.which == "27") {
+  //     $(".cd-popup").removeClass("is-visible");
+  //   }
+  // });
+});
